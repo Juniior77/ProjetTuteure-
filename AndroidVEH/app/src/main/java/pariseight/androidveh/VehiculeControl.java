53752a546dc2988c855059922e8c4a25fad24e98
Juniior77 <vilaire.guillaume@gmail.com>
2017-02-28 16:03:09 +0100

    Création Appli
    Création de l’appli projetTuteuré:
    
    Deux activity:
    
    - 1er Affiche la liste des apareil BT
    - 2nd Activity vide pour controle du vehicule

---
+++ AndroidVEH/app/src/main/java/pariseight/androidveh/VehiculeControl.java
@@ -0,0 +1,13 @@
+package pariseight.androidveh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VehiculeControl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicule_control);
+    }
+}
