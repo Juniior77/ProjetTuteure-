b938eb7424464da18996f3db0b5631541e5d8655
Juniior77 <vilaire.guillaume@gmail.com>
2017-02-28 16:54:41 +0100

    Activity 2
    Mise en place de la connection et de l’envoie de donner via capteur
    accelerometre

---
+++ AndroidVEH/app/src/main/java/pariseight/androidveh/VehiculeControlView.java
@@ -0,0 +1,29 @@
+package pariseight.androidveh;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class VehiculeControlView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public VehiculeControlView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}
