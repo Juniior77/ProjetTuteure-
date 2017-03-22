699bddeb73b0a0829a547ee27600e35b9bca9abc
totoss <totoss936@gmail.com>
2017-03-22 20:19:28 +0100

    Update

--- Arduino/ProjetTuteure/ProjetTuteure.ino
+++ Arduino/ProjetTuteure/ProjetTuteure.ino
@@ -25,26 +25,16 @@ CRGB leds[4];
 #define LIGHT_RIGHT_1_PIN A3 //attach the right  second Tracking module pinA0 to A3
 #define LIGHT_RIGHT_2_PIN A4 //attach the right first Tracking module pinA0 to A4
 
-<<<<<<< HEAD
-//byte sensorValue[5];
 
-byte data[4];            //Variable for storing received data
-
-=======
-<<<<<<< HEAD
 byte dataIn[16];            //Variable for storing received data
-=======
-byte sensorValue[5];
 
-byte data[4];            //Variable for storing received data
->>>>>>> origin/Totoss-Path
->>>>>>> origin/Totoss-Path
 int dataAvAr;
 int dataMotor1;
 int dataMotor2;
 int dataServoDir;
 
 int BLACK = 758;
+
 
 void setup()
 {
@@ -71,8 +61,6 @@ void setup()
     FastLED.show();
     Serial.print("Module prêt ! \n");
 }
-
-
 void CAR_move(int direction, int speed_left, int speed_right, int direct)
 {
     switch(direction)
@@ -132,98 +120,14 @@ void followline(){
   }
 }
 
-/*
-void sendsensorsvalue(){
-      for(int i=0; i<5; i++){ 
-      Serial.print(sensorValue[i]);
-      //Serial.print('+');
-      }
-      Serial.print('~');
-      delay(10);
-}
-*/
-
-<<<<<<< HEAD
-void loop(){
-   if(Serial.available() > 0){      // Send data only when you receive data:
-    
-=======
 void loop()
 {
   Serial.flush();
    if(Serial.available())      // Send data only when you receive data:
    {
-<<<<<<< HEAD
       Serial.readBytes(dataIn, 16);        //Read the incoming data & store into data
 
       for(int i = 0; i < 16; i++)
-=======
->>>>>>> origin/Totoss-Path
-    digitalWrite(LED_BUILTIN, HIGH);
-    
-    Serial.readBytes(data, 4);        //Read the incoming data & store into data
-    dataAvAr = data[0];
-    dataMotor1 = data[1];
-    dataMotor2 = data[2];
-    dataServoDir = data[3]; 
-
-    //A décommenter pour le suivi de ligne
-    //followline()
-
-    if(dataAvAr <= 255 && dataAvAr >127){
-      dataAvAr -= 128;
-    }else{
-      dataAvAr += 128;
-    }
-      
-    if(dataMotor1 <= 255 && dataMotor1 >127){
-      dataMotor1 -= 128;
-    }else{
-      dataMotor1 += 128;
-    }
-
-/*      
-Serial.print("Moteur1: ");
-Serial.print(dataMotor1);
-Serial.print("\n");
-*/      
-      
-<<<<<<< HEAD
-    if(dataMotor2 <= 255 && dataMotor2 >127){
-      dataMotor2 -= 128;
-    }else{
-      dataMotor2 += 128;
-    }
-
-/*
-Serial.print("Moteur2: ");
-Serial.print(dataMotor2);
-Serial.print("\n");
-*/    
-      
-    if(dataServoDir <= 255 && dataServoDir >127){
-      dataServoDir -= 128;
-    }else{
-      dataServoDir += 128;
-    }
-
-/*
-Serial.print("Servo: ");
-Serial.print(dataServoDir);
-Serial.print("\n");
-*/
-              
-  digitalWrite(LED_BUILTIN, LOW);
-  
-  CAR_move(dataAvAr, dataMotor1, dataMotor2, dataServoDir);
-  }
-=======
-      if(dataMotor2 <= 255 && dataMotor2 >127)
-      {
-        dataMotor2 -= 128;
-      }
-      else
->>>>>>> origin/Totoss-Path
       {
           if(dataIn[i] <= 255 && dataIn[i] >127)
           {
@@ -235,6 +139,8 @@ Serial.print("\n");
           }
       }
 
+      //followline();
+
       
       dataAvAr = dataIn[0];
       dataMotor1 = dataIn[1];
@@ -243,23 +149,20 @@ Serial.print("\n");
 
       leds[0].r = dataIn[4];
       leds[1].r = dataIn[5];
-      leds[2].r = dataIn[6];
-      leds[3].r = dataIn[7];
+      leds[3].r = dataIn[6];
+      leds[2].r = dataIn[7];
 
       leds[0].g = dataIn[8];
       leds[1].g = dataIn[9];
-      leds[2].g = dataIn[10];
-      leds[3].g = dataIn[11];
+      leds[3].g = dataIn[10];
+      leds[2].g = dataIn[11];
 
       leds[0].b = dataIn[12];
       leds[1].b = dataIn[13];
-      leds[2].b = dataIn[14];
-      leds[3].b = dataIn[15];
+      leds[3].b = dataIn[14];
+      leds[2].b = dataIn[15];
      
       CAR_move(dataAvAr, dataMotor1, dataMotor2, dataServoDir);
       FastLED.show();
    }
->>>>>>> origin/Totoss-Path
 }
-
-
